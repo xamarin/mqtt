2a1f9b7eea926ec9ae870a4264a94e1f3098d43c
Daniel Cazzulino <daniel@cazzulino.com>
2014-04-10 22:23:46 -0300

    Added shared targets and props and empty build script. Removed unused sln Added extra icon.

---
+++ build.bat
@@ -0,0 +1,1 @@
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild %~dp0\src\NuGet.proj /verbosity:normal /nr:false /p:PackageVersion=%PackageVersion%
---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#endif
+#if RELEASE
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
