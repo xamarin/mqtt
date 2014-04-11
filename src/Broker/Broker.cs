baec700b8478a43dc24afd524f8ff66a310b1790
Daniel Cazzulino <daniel@cazzulino.com>
2014-04-11 01:13:43 -0300

    Added sample broker class to showcase symbol integration. Made default namespace on all projects be just Hermes.

---
+++ src/Broker/Broker.cs
@@ -0,0 +1,22 @@
+﻿/*
+   Copyright 2012 Clarius Consulting
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+*/
+namespace Hermes
+{
+	using System;
+
+	public class Broker
+	{
+		public Broker(int port)
+		{
+			if (port != 1833)
+				throw new ArgumentException("Must always be 1833");
+		}
+	}
+}
