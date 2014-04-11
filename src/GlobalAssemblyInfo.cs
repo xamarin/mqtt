de6408700c1776c88c14922ee4c44b342a9b66a7
Daniel Cazzulino <daniel@cazzulino.com>
2014-04-11 00:57:49 -0300

    Remove AssemblyFileVersion which is not patched nor used automatically by nuget or myget

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -23,6 +23,5 @@ using System.Runtime.CompilerServices;
 #endif
 
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
 // This attribute is used for the NuGet package version.
 [assembly: AssemblyInformationalVersion("0.1.0-pre")]
