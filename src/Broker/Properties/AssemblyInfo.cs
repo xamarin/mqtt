4f45e0fbe1b08d00a47adcae2abcc1fe1515883e
Daniel Cazzulino <daniel@cazzulino.com>
2014-04-10 22:39:40 -0300

    Added default license headers. Added release notes.

--- src/Broker/Properties/AssemblyInfo.cs
+++ src/Broker/Properties/AssemblyInfo.cs
@@ -1,4 +1,13 @@
-﻿using System.Reflection;
+﻿/*
+   Copyright 2014 Clarius Consulting
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+*/
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
