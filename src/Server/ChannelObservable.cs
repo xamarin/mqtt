f56ff0cabbc01d693fa7c5aecee138e9f3052853
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2015-04-09 01:21:03 -0300

    Added a way to close the TcpListener when the Server is disposed

---
+++ src/Server/ChannelObservable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Reactive.Disposables;
+
+namespace Hermes
+{
+	public class ChannelObservable : IObservable<IChannel<byte[]>>
+	{
+		private readonly TcpListener listener;
+		private readonly IObservable<IChannel<byte[]>> innerObservable;
+
+		public ChannelObservable (TcpListener listener, IObservable<IChannel<byte[]>> innerObservable)
+		{
+			this.listener = listener;
+			this.innerObservable = innerObservable;
+		}
+
+		public IDisposable Subscribe (IObserver<IChannel<byte[]>> observer)
+		{
+			return new CompositeDisposable (innerObservable.Subscribe (observer),
+				Disposable.Create (() => this.listener.Stop ()));
+		}
+	}
+}
