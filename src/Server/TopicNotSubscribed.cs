5dcc67ea83ad8e844e16c434e24a6ce3cd7b6d5b
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2015-05-05 17:14:58 -0300

    Added Payload to TopicNotSubscribed event

--- src/Server/TopicNotSubscribed.cs
+++ src/Server/TopicNotSubscribed.cs
@@ -1,9 +1,13 @@
-﻿namespace Hermes
+﻿using Hermes.Packets;
+
+namespace Hermes
 {
 	public class TopicNotSubscribed
 	{
 		public string Topic { get; set; }
 
 		public string SenderId { get; set; }
+
+		public byte[] Payload { get; set; }
 	}
 }
