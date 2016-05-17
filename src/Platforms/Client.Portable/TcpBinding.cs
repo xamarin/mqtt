dade8ac70695212c83ea6efdb668fde1b540e77b
Daniel Cazzulino <daniel@cazzulino.com>
2016-05-17 02:22:29 -0300

    Added missing Client/Server portable API surface

---
+++ src/Platforms/Client.Portable/TcpBinding.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Mqtt.Diagnostics;
+
+namespace System.Net.Mqtt
+{
+	public class TcpBinding : IProtocolBinding
+	{
+		public IChannelFactory GetChannelFactory(string hostAddress, ITracerManager tracerManager, ProtocolConfiguration configuration)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IChannelProvider GetChannelProvider(ITracerManager tracerManager, ProtocolConfiguration configuration)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
