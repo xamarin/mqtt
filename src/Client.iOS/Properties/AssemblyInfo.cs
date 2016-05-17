62b1934f881bc3c5081cf8c85c9a9ad82c615ab8
Daniel Cazzulino <daniel@cazzulino.com>
2016-05-17 00:41:12 -0300

    Moved platform-specific projects under Platforms and added WP/WinRT

--- src/Client.iOS/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle ("System.Net.Mqtt.iOS")]
-[assembly: AssemblyDescription ("Mqtt client library for iOS projects.")]
--- src/Server.iOS/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle ("System.Net.Mqtt.Server.iOS")]
-[assembly: AssemblyDescription ("A lightweight and high-performance MQTT server/broker for iOS.")]
