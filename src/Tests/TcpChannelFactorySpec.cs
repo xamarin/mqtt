975df1b7e1d5c3dd06a392b770d80b8499d6acc4
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2017-05-29 14:18:51 -0300

    Fixed issue in TcpChannelFactory
    - After latest changes adding connection timeout, an issue has been introduced which swallows any exception that occurs as part of the initial TCP connection
    - This fix allows to re throw those exceptions
    - Also, this fix adds missing unit tests over TcpChannelFactory

---
+++ src/Tests/TcpChannelFactorySpec.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Mqtt;
+using System.Net.Mqtt.Sdk.Bindings;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+	public class TcpChannelFactorySpec
+	{
+		[Fact]
+		public async Task when_creating_channel_then_succeeds()
+		{
+			var configuration = new MqttConfiguration { ConnectionTimeoutSecs = 2 };
+			var listener = new TcpListener (IPAddress.Loopback, configuration.Port);
+
+			listener.Start ();
+
+			var factory = new TcpChannelFactory (IPAddress.Loopback.ToString (), configuration);
+			var channel = await factory.CreateAsync ();
+
+			Assert.NotNull (channel);
+			Assert.True (channel.IsConnected);
+
+			listener.Stop ();
+		}
+
+		[Fact]
+		public void when_creating_channel_with_invalid_address_then_fails()
+		{
+			var configuration = new MqttConfiguration { ConnectionTimeoutSecs = 2 };
+			var factory = new TcpChannelFactory (IPAddress.Loopback.ToString (), configuration);
+
+			var ex = Assert.Throws<AggregateException> (() => factory.CreateAsync ().Result);
+
+			Assert.NotNull (ex);
+			Assert.NotNull (ex.InnerException);
+			Assert.True (ex.InnerException is MqttException);
+			Assert.NotNull (ex.InnerException.InnerException);
+			Assert.True (ex.InnerException.InnerException is SocketException);
+		}
+	}
+}
